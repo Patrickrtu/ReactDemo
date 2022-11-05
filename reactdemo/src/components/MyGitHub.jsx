6f368996f78ae7ee2dad3061b75b1331f85f006c
Patrick Tung <83930287+Patrickrtu@users.noreply.github.com>
2022-11-05 14:15:23 -0600

    Added MyGitHub.jsx

---
+++ reactdemo/src/components/MyGitHub.jsx
@@ -0,0 +1,13 @@
+const MyGitHub = () => {
+  return (
+    <p>
+      Link to GitHub:{" "}
+      <a href="https://github.com/Patrickrtu/ReactDemo.git">
+        {" "}
+        https://github.com/Patrickrtu/ReactDemo.git
+      </a>
+    </p>
+  );
+};
+export default MyGitHub;
+
